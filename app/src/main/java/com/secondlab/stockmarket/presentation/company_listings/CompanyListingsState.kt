dcf40e16e3a4252c6287db100f4d7f11c77a8277
Rizki Rian Anandita <65359346+rizriana@users.noreply.github.com>
2022-09-12 01:05:02 +0700

    Company listings view model

---
+++ app/src/main/java/com/secondlab/stockmarket/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.secondlab.stockmarket.presentation.company_listings
+
+import com.secondlab.stockmarket.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = "",
+)
